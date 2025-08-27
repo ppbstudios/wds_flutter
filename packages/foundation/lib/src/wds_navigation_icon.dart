48d509b97f3bbe057ac0b28630f0fcb3cffcae1f
seunghwanly <seunghwanly@gmail.com>
2025-08-27 17:09:28 +0900

    Feature: 내비게이션 아이콘 및 벡터 자산 추가
    * 새로운 내비게이션 아이콘을 위한 SVG 파일 추가 (활성 및 비활성 상태)
    * 기존 chevronRight.svg 파일을 chevron_right.svg로 이름 변경
    * WdsIcon 및 WdsNavigationIcon 열거형에 새로운 아이콘 추가
    * pubspec.yaml에 새로운 자산 경로 추가

---
+++ packages/foundation/lib/src/wds_navigation_icon.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+
+enum WdsNavigationIcon {
+  store(
+    inactivePath: 'assets/vector/navigation/store_inactive.svg',
+    activePath: 'assets/vector/navigation/store_active.svg',
+  ),
+  home(
+    inactivePath: 'assets/vector/navigation/home_inactive.svg',
+    activePath: 'assets/vector/navigation/home_active.svg',
+  ),
+  like(
+    inactivePath: 'assets/vector/navigation/like_inactive.svg',
+    activePath: 'assets/vector/navigation/like_active.svg',
+  ),
+  category(
+    inactivePath: 'assets/vector/navigation/category_inactive.svg',
+    activePath: 'assets/vector/navigation/category_active.svg',
+  ),
+  my(
+    inactivePath: 'assets/vector/navigation/my_inactive.svg',
+    activePath: 'assets/vector/navigation/my_active.svg',
+  );
+
+  const WdsNavigationIcon({
+    required this.activePath,
+    required this.inactivePath,
+  });
+
+  final String activePath;
+
+  final String inactivePath;
+
+  Widget build({
+    bool isActive = false,
+  }) =>
+      SvgPicture.asset(
+        isActive ? activePath : inactivePath,
+      );
+}
