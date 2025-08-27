bb9193b1a23931bbb728f94e28a97aabff48397e
seunghwanly <seunghwanly@gmail.com>
2025-08-27 17:57:00 +0900

    Fix: dimmed 투명도 조정

--- packages/tokens/lib/semantic/color/wds_semantic_color_material.dart
+++ packages/tokens/lib/semantic/color/wds_semantic_color_material.dart
@@ -3,6 +3,6 @@ part of '../color.dart';
 
 class WdsSemanticColorMaterial {
   const WdsSemanticColorMaterial._();
-  static const Color dimmer = Color(0xCC000000);
+  static const Color dimmer = Color(0x99000000);
   static const Color pressed = Color(0x1A000000);
 }
