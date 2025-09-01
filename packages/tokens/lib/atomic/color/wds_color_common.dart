3aeb6edad855b8c2f64032d8512c5609a7553208
seunghwanly <seunghwanly@gmail.com>
2025-09-01 18:31:24 +0900

    Fix: 토큰 재생성

--- packages/tokens/lib/atomic/color/wds_color_common.dart
+++
@@ -1,8 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-part of '../color.dart';
-
-class WdsColorCommon {
-  const WdsColorCommon._();
-  static const Color black = Color(0xFF000000);
-  static const Color white = Color(0xFFFFFFFF);
-}
