6d684bba6c4dbdbdb93ac6b592d10e274d72e501
yejin <7602yejin@gmail.com>
2025-09-02 16:05:12 +0900

    Feature: PaginationCount > 컴포넌트 생성

--- packages/components/lib/wds_components.dart
+++ packages/components/lib/wds_components.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart'
         TextSelectionThemeData;
 import 'package:flutter/widgets.dart';
 import 'package:wds_foundation/wds_foundation.dart';
+import 'package:wds_tokens/wds_tokens.dart';
 
 part 'src/action_area/wds_action_area.dart';
 part 'src/button/button.dart';
@@ -20,6 +21,7 @@ part 'src/chip/wds_chip.dart';
 part 'src/header/wds_header.dart';
 part 'src/icon/wds_icon_button.dart';
 part 'src/navigation/wds_bottom_navigation.dart';
+part 'src/pagination/wds_count_pagination.dart';
 part 'src/pagination/wds_dot_pagination.dart';
 part 'src/select/wds_select.dart';
 part 'src/switch/wds_switch.dart';
