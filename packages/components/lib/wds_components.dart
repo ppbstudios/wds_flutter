5e2e995b7d8afc7412ba37ad9573486133c707b3
seunghwanly <seunghwanly@gmail.com>
2025-09-04 15:33:03 +0900

    Feat: WdsDotBadge 컴포넌트 추가 및 믹스인, 확장 메서드 구현

--- packages/components/lib/wds_components.dart
+++ packages/components/lib/wds_components.dart
@@ -13,6 +13,7 @@ import 'package:wds_foundation/wds_foundation.dart';
 import 'package:wds_tokens/wds_tokens.dart';
 
 part 'src/action_area/wds_action_area.dart';
+part 'src/badge/wds_dot_badge.dart';
 part 'src/button/button.dart';
 part 'src/button/wds_square_button.dart';
 part 'src/button/wds_text_button.dart';
