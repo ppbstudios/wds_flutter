9da90af65eccf279f9266c07e40d50762b35bd36
seunghwanly <seunghwanly@gmail.com>
2025-09-10 10:22:13 +0900

    Feat: 숫자 확장 메서드 추가 및 의존성 업데이트
    - pubspec.yaml에 intl 패키지 추가
    - WdsHelper에 num_extension.dart 파일을 추가하여 숫자 포맷 및 할인율 계산 기능 구현
    - WdsFoundation 라이브러리에 num_extension.dart를 export하여 사용 가능하도록 설정
    - WdsNavigationIcon에서 아이콘의 색상, 너비, 높이 매개변수 추가하여 커스터마이징 지원

---
+++ packages/foundation/lib/src/helper/wds_helper.dart
@@ -0,0 +1,3 @@
+import 'package:intl/intl.dart';
+
+part 'num_extension.dart';
--- packages/foundation/lib/wds_foundation.dart
+++ packages/foundation/lib/wds_foundation.dart
@@ -1,5 +1,6 @@
 library;
 
+export 'src/helper/wds_helper.dart';
 export 'src/wds_colors.dart';
 export 'src/wds_icon.dart';
 export 'src/wds_opacity.dart';
