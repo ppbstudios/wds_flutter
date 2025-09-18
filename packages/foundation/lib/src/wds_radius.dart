c3a0cd24c74635a7ddc6af5c4b598086e6f2be46
seunghwanly <seunghwanly@gmail.com>
2025-09-18 16:25:53 +0900

    Fix: WdsRadius 및 WdsSpacing의 atomic 토큰 값 업데이트, WdsTypography의 caption 스타일 이름 변경

--- packages/foundation/lib/src/wds_radius.dart
+++ packages/foundation/lib/src/wds_radius.dart
@@ -5,26 +5,26 @@ import 'package:wds_tokens/atomic/wds_atomic_radius.dart';
 class WdsRadius {
   const WdsRadius._();
 
-  static const double none = WdsAtomicRadius.none;
+  static const double none = WdsAtomicRadius.v0;
 
   /// 4px
-  static const double xs = WdsAtomicRadius.xs;
+  static const double xs = WdsAtomicRadius.v4;
 
   /// 8px
-  static const double sm = WdsAtomicRadius.sm;
+  static const double sm = WdsAtomicRadius.v8;
 
   /// 12px
-  static const double md = WdsAtomicRadius.md;
+  static const double md = WdsAtomicRadius.v12;
 
   /// 16px
-  static const double lg = WdsAtomicRadius.lg;
+  static const double lg = WdsAtomicRadius.v16;
 
   /// 20px
-  static const double xl = WdsAtomicRadius.xl;
+  static const double xl = WdsAtomicRadius.v20;
 
   /// 30px
-  static const double xxl = WdsAtomicRadius.xxl;
+  static const double xxl = WdsAtomicRadius.v30;
 
   /// 9999px
-  static const double full = WdsAtomicRadius.full;
+  static const double full = WdsAtomicRadius.v9999;
 }
