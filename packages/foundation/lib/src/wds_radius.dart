f6c53205b626fa881a6acdc971fda705c4f9ce8c
seunghwanly <seunghwanly@gmail.com>
2025-09-19 09:43:03 +0900

    Fix: foundation 토큰 정리

--- packages/foundation/lib/src/wds_radius.dart
+++ packages/foundation/lib/src/wds_radius.dart
@@ -5,26 +5,26 @@ import 'package:wds_tokens/atomic/wds_atomic_radius.dart';
 class WdsRadius {
   const WdsRadius._();
 
-  static const double none = WdsAtomicRadius.v0;
+  static const double radius0 = WdsAtomicRadius.v0;
 
   /// 4px
-  static const double xs = WdsAtomicRadius.v4;
+  static const double radius4 = WdsAtomicRadius.v4;
 
   /// 8px
-  static const double sm = WdsAtomicRadius.v8;
+  static const double radius8 = WdsAtomicRadius.v8;
 
   /// 12px
-  static const double md = WdsAtomicRadius.v12;
+  static const double radius12 = WdsAtomicRadius.v12;
 
   /// 16px
-  static const double lg = WdsAtomicRadius.v16;
+  static const double radius16 = WdsAtomicRadius.v16;
 
   /// 20px
-  static const double xl = WdsAtomicRadius.v20;
+  static const double radius20 = WdsAtomicRadius.v20;
 
   /// 30px
-  static const double xxl = WdsAtomicRadius.v30;
+  static const double radius30 = WdsAtomicRadius.v30;
 
   /// 9999px
-  static const double full = WdsAtomicRadius.v9999;
+  static const double radius9999 = WdsAtomicRadius.v9999;
 }
